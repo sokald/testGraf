98358633ff71c962e767263aed2321ad279bbdfb
sokald <maciekb00@gmail.com>
2019-06-17 22:54:35 +0200

    commit

---
+++ testGraf/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using OxyPlot.Wpf;
+
+namespace testGraf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ViewModels.MainWindowModel viewModel;
+
+        //varibles for calculate result
+        int a, b, c;
+
+        //varibles for set time refresh
+        int time = 2000;
+
+        public MainWindow()
+        {
+            viewModel = new ViewModels.MainWindowModel();
+            DataContext = viewModel;
+            
+            InitializeComponent();
+            
+            // loop for calculate multiplication
+            try
+            {
+                var i = loop();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("balad dzialania prgramu " + e);
+            }
+        }
+
+        // loop for calculate and show result
+        public async Task<int> loop()
+        {
+
+            //object for random number
+            Random rnd = new Random();
+            int i = 5;
+
+            while (true)
+            {
+                //code for calculate and show result
+                try
+                {
+                    a = Int32.Parse(aTextBox.Text);
+                    b = Int32.Parse(bTextBox.Text);
+                    c = await calculate(a, b);
+                    //progressBar.Value = c;
+                    resultLabel.Content = "= " + c.ToString() + "+" + rnd.NextDouble();
+
+                    //add new number in plot       
+                    i += 5;
+                    viewModel.add(new OxyPlot.DataPoint(i, c));
+                    Plot1.RefreshPlot(true);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("blad dzialanai programu" + e);
+                }
+            }
+            return 0;
+        }  
+
+        // button for new number w plot
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            viewModel.add();
+            Plot1.RefreshPlot(true);
+        }
+
+        //button to set time refresh
+        private void Apply_Click(object sender, RoutedEventArgs e)
+        {
+            time = Int32.Parse(setTimeLabler.Text);
+        }
+
+        // calculate random number
+        public async Task<int> calculate(int a, int b)
+        {
+            return await Task.Run(() =>
+            {
+                Thread.Sleep(time);
+                return a * b;
+            });
+        }
+    }
+}
---
+++ testGraf/ViewModels/MainWindowModel.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using OxyPlotDemo.Annotations;
+using OxyPlotDemo;
+
+namespace testGraf.ViewModels
+{
+    class MainWindowModel : INotifyPropertyChanged
+    {
+        private PlotModel plotModel;
+        public PlotModel PlotModel
+        {
+            get { return plotModel; }
+            set { plotModel = value; OnPropertyChanged("PlotModel"); }
+        }
+
+        private DateTime lastUpdate = DateTime.Now;
+
+        OxyPlot.Series.LineSeries punktySerii;
+
+        public MainWindowModel()
+        {
+            this.plotModel = new PlotModel { Title = "Example 1" };
+            //this.plotModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+            
+            punktySerii = new LineSeries();
+            //punktySerii.Points.Add(new OxyPlot.DataPoint(5, 3));
+            //punktySerii.Points.Add(new OxyPlot.DataPoint(15, 17));
+            //punktySerii.Points.Add(new OxyPlot.DataPoint(25, 12));
+            //punktySerii.Points.Add(new OxyPlot.DataPoint(35, 4));
+            //punktySerii.Points.Add(new OxyPlot.DataPoint(45, 15));
+            //punktySerii.Points.Add(new OxyPlot.DataPoint(55, 10));
+
+            plotModel.Series.Add(punktySerii);
+
+            //PlotModel = new PlotModel();
+            //SetUpModel();
+            //LoadData();
+        }
+
+        Random r = new Random();
+
+        public void add()
+        {
+            punktySerii.Points.Add(new OxyPlot.DataPoint(r.Next(100), r.Next(100)));
+        }
+
+        public void add(OxyPlot.DataPoint point)
+        {
+            punktySerii.Points.Add(point);
+            //punktySerii.Points.Add(new OxyPlot.DataPoint(r.Next(100), r.Next(100)));
+        }
+
+        private void LoadData()
+        {
+            List<Measurement> measurements = Data.GetData();
+
+            var dataPerDetector = measurements.GroupBy(m => m.DetectorId).OrderBy(m => m.Key).ToList();
+
+            foreach (var data in dataPerDetector)
+            {
+                var lineSerie = new LineSeries
+                {
+                    StrokeThickness = 2,
+                    MarkerSize = 3,
+                    MarkerStroke = OxyColors.Green,
+                    MarkerType = MarkerType.Star,
+                    CanTrackerInterpolatePoints = false,
+                    Title = string.Format("Detector {0}", data.Key),
+                    Smooth = false,
+                };
+
+                data.ToList().ForEach(d => lineSerie.Points.Add(new DataPoint(DateTimeAxis.ToDouble(d.DateTime), d.Value)));
+                PlotModel.Series.Add(lineSerie);
+            }
+            lastUpdate = DateTime.Now;
+        }
+
+        private void SetUpModel()
+        {
+            PlotModel.LegendTitle = "Legend";
+            PlotModel.LegendOrientation = LegendOrientation.Horizontal;
+            PlotModel.LegendPlacement = LegendPlacement.Outside;
+            PlotModel.LegendPosition = LegendPosition.TopRight;
+            PlotModel.LegendBackground = OxyColor.FromAColor(200, OxyColors.White);
+            PlotModel.LegendBorder = OxyColors.Black;
+
+            var dateAxis = new DateTimeAxis(AxisPosition.Bottom, "Date", "HH:mm") { MajorGridlineStyle = LineStyle.Solid, MinorGridlineStyle = LineStyle.Dot, IntervalLength = 80 };
+            PlotModel.Axes.Add(dateAxis);
+            var valueAxis = new LinearAxis(AxisPosition.Left, 0) { MajorGridlineStyle = LineStyle.Solid, MinorGridlineStyle = LineStyle.Dot, Title = "Value" };
+            PlotModel.Axes.Add(valueAxis);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
